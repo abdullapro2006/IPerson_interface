99a33cb48ab4a5cc38e930772fbbb61f4d47506d
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-04 18:00:37 +0400

    IPerson interface created

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public interface IPerson 
+{
+    void Speak();
+}
+public  class RussianPerson :IPerson
+{
+    public void Speak()
+    {
+        Console.WriteLine("Privet");
+    }
+}
+public class FrenchPerson :IPerson
+{
+    public void Speak() 
+    {
+        Console.WriteLine("Bonjour");
+    }
+}
+public class SpanishPerson 
+{ 
+    public void Speak()
+    {
+        Console.WriteLine("Hola");
+    }
+}
+public class Program 
+{ 
+    public static void Main(string[] args)
+    {
+        var russianperson = new RussianPerson();
+        russianperson.Speak();
+        var spanishperson = new SpanishPerson();
+        spanishperson.Speak();
+        var frenchperson = new FrenchPerson();
+        frenchperson.Speak();
+        
+    }
+}
+
+
+
+
+
+
